49769cbac886b90ad217445b3fc1b81cb2291f76
nav singh <navtej927@gmail.com>
2024-08-21 21:23:39 +0000

    honecomb added package

--- back-end/notification-api/src/index.mjs
+++ back-end/notification-api/src/index.mjs
@@ -1,10 +1,19 @@
 import express from "express";
+import opentelemetry from "@opentelemetry/api";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.status(200).json({
         name: "notification-api v2"
+    })
+})
+
+const tracer = opentelemetry.trace.getTracer("notification-api-temp");
+
+app.get("/api", (req, res) => {
+    res.status(200).json({
+        name: "notification-api"
     })
 })
 
