74a765dcc69829c39b19daf86f9c452b5732d9c4
nav singh <navtej927@gmail.com>
2024-08-22 10:28:27 +0000

    update

--- back-end/notification-api/src/tracing.js
+++ back-end/notification-api/src/tracing.js
@@ -1,21 +1,21 @@
-const { HoneycombSDK } = require('@honeycombio/opentelemetry-node');
+// Example filename: tracing.js
+'use strict';
+
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-http');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const opentelemetry = require('@opentelemetry/api');
 
-const sdk = new HoneycombSDK({
-    instrumentations: [getNodeAutoInstrumentations({
-        '@opentelemetry/instrumentation-fs': {
-            enabled: false,
-        },
-    },
-    new HttpInstrumentation()
-)]
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter: new OTLPTraceExporter(),
+    instrumentations: [
+        getNodeAutoInstrumentations({
+            // we recommend disabling fs autoinstrumentation since it can be noisy
+            // and expensive during startup
+            '@opentelemetry/instrumentation-fs': {
+                enabled: false,
+            },
+        }),
+    ],
 });
-
-//
-// Global tracer for this service.
-//
-const tracer = opentelemetry.trace.getTracer(process.env.OTEL_SERVICE_NAME);
 
 sdk.start();
